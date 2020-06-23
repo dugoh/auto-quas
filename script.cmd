15d827c528afac9a6415634af55800c8c84400b4
Jacob Goense <dugo@xs4all.nl>
2020-06-23 13:43:16 +0200

    Update script.cmd

--- script.cmd
+++ script.cmd
@@ -4,10 +4,10 @@ build_vstudio.bat BuildROMs VAX|more
 echo
 echo ++++++++++ done1
 type c:\projects\hello\simh\BIN\NT\Project\simh\VAX\Win32-Release\BuildLog.htm
-echo
+echo -
 echo ++++++++++ done2
 type c:\projects\hello\simh\BIN\NT\Project\simh\BuildROMs\Win32-Release\BuildLog.htm
-echo
+echo -
 echo ++++++++++ done3
 cd ..
 copy C:\projects\hello\simh\BIN\NT\Win32-Release\MicroVAX3900.exe .\
@@ -15,6 +15,8 @@ echo sh ver| microvax3900
 echo ++++++++++ done4
 SET OPATH=%PATH%
 SET PATH=c:\MinGW\bin;c:\MinGW\usr\bin;%OPATH%
+DIR C:\
+echo %PATH%
 gcc -o hello.exe hello.c
 echo ++++++++++ done5
 hello.exe
