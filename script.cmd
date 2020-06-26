5d90171f638e1f77e9ff1eaf1ca73aa0d91cdb8a
Jacob Goense <dugo@xs4all.nl>
2020-06-26 12:54:45 +0200

    Update script.cmd

--- script.cmd
+++ script.cmd
@@ -1,31 +1,32 @@
-git clone https://github.com/simh/simh.git
-cd simh
-pwd
-echo bashmaybe $SHELL
-REM comment
-dir /s /b
-build_vstudio.bat BuildROMs VAX|more
-echo
-echo ++++++++++ done1
-type c:\projects\hello\simh\BIN\NT\Project\simh\VAX\Win32-Release\BuildLog.htm
-echo -
-echo ++++++++++ done2
-type c:\projects\hello\simh\BIN\NT\Project\simh\BuildROMs\Win32-Release\BuildLog.htm
-echo -
-echo ++++++++++ done3
-cd ..
-copy C:\projects\hello\simh\BIN\NT\Win32-Release\MicroVAX3900.exe .\
-echo sh ver| microvax3900
-echo ++++++++++ done4
 DIR C:\
-echo %PATH%
-SET OPATH=%PATH%
-SET PATH=c:\MinGW\bin;c:\MinGW\usr\bin;%OPATH%
-DIR C:\
-echo %PATH%
-gcc -o hello.exe hello.c
-echo ++++++++++ done5
-hello.exe
-echo ++++++++++ done6
-perl -e 'print "hello world!\n"'
-echo ++++++++++ done7
+REM git clone https://github.com/simh/simh.git
+REM cd simh
+REM pwd
+REM echo bashmaybe $SHELL
+REM REM comment
+REM dir /s /b
+REM build_vstudio.bat BuildROMs VAX|more
+REM echo
+REM echo ++++++++++ done1
+REM type c:\projects\hello\simh\BIN\NT\Project\simh\VAX\Win32-Release\BuildLog.htm
+REM echo -
+REM echo ++++++++++ done2
+REM type c:\projects\hello\simh\BIN\NT\Project\simh\BuildROMs\Win32-Release\BuildLog.htm
+REM echo -
+REM echo ++++++++++ done3
+REM cd ..
+REM copy C:\projects\hello\simh\BIN\NT\Win32-Release\MicroVAX3900.exe .\
+REM echo sh ver| microvax3900
+REM echo ++++++++++ done4
+REM DIR C:\
+REM echo %PATH%
+REM SET OPATH=%PATH%
+REM SET PATH=c:\MinGW\bin;c:\MinGW\usr\bin;%OPATH%
+REM DIR C:\
+REM echo %PATH%
+REM gcc -o hello.exe hello.c
+REM echo ++++++++++ done5
+REM hello.exe
+REM echo ++++++++++ done6
+REM perl -e 'print "hello world!\n"'
+REM echo ++++++++++ done7
