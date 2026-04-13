4127ff90bd6140dabeb62ced982d44fb6a49904f
Jacob Goense <dugo@xs4all.nl>
2026-04-13 10:39:52 +0200

    Update script.cmd

--- script.cmd
+++ script.cmd
@@ -16,9 +16,7 @@ build_vstudio.bat BuildROMs VAX|more
 CD ..
 COPY C:\projects\auto-quas\simh\BIN\NT\Win32-Release\MicroVAX3900.exe C:\projects\auto-quas\simh\BIN
 SET OPATH=%PATH%
-REM SET PATH=c:\MinGW\bin;c:\MinGW\usr\bin;.;%OPATH%
-REM guessing again
-SET "PATH=C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin;%OPATH%"
+SET PATH=C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin;%OPATH%;C:\Program Files\Git\bin;C:\Program Files\Git\usr\bin
 
 bash -c './script buildgzcompatwin'
 bash -c './script maketap'
