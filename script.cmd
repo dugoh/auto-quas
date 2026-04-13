9e68ec237e5390b909715a7536ea3833794796b2
Jacob Goense <dugo@xs4all.nl>
2026-04-13 15:20:24 +0200

    Update script.cmd a

--- script.cmd
+++ script.cmd
@@ -1,13 +1,13 @@
-git clone https://github.com/simh/simh.git
-CD simh
-git checkout %1
-build_vstudio.bat BuildROMs VAX|more
-CD ..
-COPY C:\projects\auto-quas\simh\BIN\NT\Win32-Release\MicroVAX3900.exe C:\projects\auto-quas\simh\BIN
+rem git clone https://github.com/simh/simh.git
+rem CD simh
+rem git checkout %1
+rem build_vstudio.bat BuildROMs VAX|more
+rem CD ..
+rem COPY C:\projects\auto-quas\simh\BIN\NT\Win32-Release\MicroVAX3900.exe C:\projects\auto-quas\simh\BIN
 SET OPATH=%PATH%
 SET PATH=C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin;%OPATH%;C:\Program Files\Git\bin;C:\Program Files\Git\usr\bin
 
 bash -c './script buildgzcompatwin'
 bash -c './script maketap'
-bash -c './script writeinis'
-bash -c './script runit'
+rem bash -c './script writeinis'
+rem bash -c './script runit'
